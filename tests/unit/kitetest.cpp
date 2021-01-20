675beb371365863bd2ab721134eda52ec4827c13
Bhumit Attarde <bhumit.attarde01@gmail.com>
2021-01-20 11:13:59 +0530

    added order test cases

--- tests/unit/kitetest.cpp
+++ tests/unit/kitetest.cpp
@@ -117,3 +117,163 @@ TEST(kiteTest, profile) {
     EXPECT_THAT(profile.orderTypes, ::testing::ElementsAre("LIMIT", "MARKET", "SL", "SL-M"));
     EXPECT_THAT(profile.exchanges, ::testing::ElementsAre("BSE", "BFO", "NFO", "MCX", "CDS", "NSE"));
 };
+
+TEST(kiteTest, getMarginsTest1) {
+
+    std::ifstream jsonFile("../../tests/mock_responses/margins.json");
+    ASSERT_TRUE(jsonFile);
+    rj::IStreamWrapper jsonFWrap(jsonFile);
+
+    mockKite Kite;
+
+    EXPECT_CALL(Kite, _sendReq(_, kitepp::_methods::GET, "/user/margins", _, _))
+        .WillOnce(testing::Invoke([&jsonFWrap](rj::Document& data, const kitepp::_methods& mtd, const string& endpoint,
+                                      const std::vector<std::pair<string, string>>& bodyParams = {},
+                                      bool isJson = false) { data.ParseStream(jsonFWrap); }));
+
+    kitepp::allMargins margins = Kite.getMargins();
+
+    // Expected values
+    EXPECT_EQ(margins.equity.enabled, true);
+    EXPECT_DOUBLE_EQ(margins.equity.net, 15481.524);
+
+    EXPECT_DOUBLE_EQ(margins.equity.available.adHocMargin, 0);
+    EXPECT_DOUBLE_EQ(margins.equity.available.cash, 9929.024);
+    EXPECT_DOUBLE_EQ(margins.equity.available.collateral, 5554.5);
+    EXPECT_DOUBLE_EQ(margins.equity.available.intradayPayin, 0);
+
+    EXPECT_DOUBLE_EQ(margins.equity.used.debits, 2);
+    EXPECT_DOUBLE_EQ(margins.equity.used.exposure, 0);
+    EXPECT_DOUBLE_EQ(margins.equity.used.M2MRealised, -2);
+    EXPECT_DOUBLE_EQ(margins.equity.used.M2MUnrealised, 0);
+    EXPECT_DOUBLE_EQ(margins.equity.used.optionPremium, 0);
+    EXPECT_DOUBLE_EQ(margins.equity.used.payout, 0);
+    EXPECT_DOUBLE_EQ(margins.equity.used.span, 0);
+    EXPECT_DOUBLE_EQ(margins.equity.used.holdingSales, 0);
+    EXPECT_DOUBLE_EQ(margins.equity.used.turnover, 0);
+
+    EXPECT_EQ(margins.commodity.enabled, true);
+    EXPECT_DOUBLE_EQ(margins.commodity.net, 29675.93);
+
+    EXPECT_DOUBLE_EQ(margins.commodity.available.adHocMargin, 0);
+    EXPECT_DOUBLE_EQ(margins.commodity.available.cash, 29249.93);
+    EXPECT_DOUBLE_EQ(margins.commodity.available.collateral, 0);
+    EXPECT_DOUBLE_EQ(margins.commodity.available.intradayPayin, 0);
+
+    EXPECT_DOUBLE_EQ(margins.commodity.used.debits, -426);
+    EXPECT_DOUBLE_EQ(margins.commodity.used.exposure, 0);
+    EXPECT_DOUBLE_EQ(margins.commodity.used.M2MRealised, 426);
+    EXPECT_DOUBLE_EQ(margins.commodity.used.M2MUnrealised, 0);
+    EXPECT_DOUBLE_EQ(margins.commodity.used.optionPremium, 0);
+    EXPECT_DOUBLE_EQ(margins.commodity.used.payout, 0);
+    EXPECT_DOUBLE_EQ(margins.commodity.used.span, 0);
+    EXPECT_DOUBLE_EQ(margins.commodity.used.holdingSales, 0);
+    EXPECT_DOUBLE_EQ(margins.commodity.used.turnover, 0);
+}
+
+TEST(kiteTest, getMarginsTest2) {
+
+    std::ifstream jsonFile("../../tests/mock_responses/margins_equity.json");
+    ASSERT_TRUE(jsonFile);
+    rj::IStreamWrapper jsonFWrap(jsonFile);
+
+    mockKite Kite;
+
+    EXPECT_CALL(Kite, _sendReq(_, kitepp::_methods::GET, _, _, _))
+        .WillOnce(testing::Invoke([&jsonFWrap](rj::Document& data, const kitepp::_methods& mtd, const string& endpoint,
+                                      const std::vector<std::pair<string, string>>& bodyParams = {},
+                                      bool isJson = false) { data.ParseStream(jsonFWrap); }));
+
+    kitepp::margins margins = Kite.getMargins("equity");
+
+    // Expected values
+    EXPECT_EQ(margins.enabled, true);
+    EXPECT_DOUBLE_EQ(margins.net, 15481.524);
+
+    EXPECT_DOUBLE_EQ(margins.available.adHocMargin, 0);
+    EXPECT_DOUBLE_EQ(margins.available.cash, 9929.024);
+    EXPECT_DOUBLE_EQ(margins.available.collateral, 5554.5);
+    EXPECT_DOUBLE_EQ(margins.available.intradayPayin, 0);
+
+    EXPECT_DOUBLE_EQ(margins.used.debits, 2);
+    EXPECT_DOUBLE_EQ(margins.used.exposure, 0);
+    EXPECT_DOUBLE_EQ(margins.used.M2MRealised, -2);
+    EXPECT_DOUBLE_EQ(margins.used.M2MUnrealised, 0);
+    EXPECT_DOUBLE_EQ(margins.used.optionPremium, 0);
+    EXPECT_DOUBLE_EQ(margins.used.payout, 0);
+    EXPECT_DOUBLE_EQ(margins.used.span, 0);
+    EXPECT_DOUBLE_EQ(margins.used.holdingSales, 0);
+    EXPECT_DOUBLE_EQ(margins.used.turnover, 0);
+}
+
+TEST(kiteTest, placeOrderTest) {
+
+    std::ifstream jsonFile("../../tests/mock_responses/order_response.json");
+    ASSERT_TRUE(jsonFile);
+    rj::IStreamWrapper jsonFWrap(jsonFile);
+
+    mockKite Kite;
+
+    EXPECT_CALL(Kite, _sendReq(_, kitepp::_methods::POST, _, _, _))
+        .WillOnce(testing::Invoke([&jsonFWrap](rj::Document& data, const kitepp::_methods& mtd, const string& endpoint,
+                                      const std::vector<std::pair<string, string>>& bodyParams = {},
+                                      bool isJson = false) { data.ParseStream(jsonFWrap); }));
+
+    string orderID = Kite.placeOrder("arg1", "arg2", "arg3", "arg4", 1, "arg6", "arg7");
+
+    EXPECT_EQ(orderID, "151220000000000");
+}
+
+TEST(kiteTest, modifyOrderTest) {
+
+    std::ifstream jsonFile("../../tests/mock_responses/order_response.json");
+    ASSERT_TRUE(jsonFile);
+    rj::IStreamWrapper jsonFWrap(jsonFile);
+
+    mockKite Kite;
+
+    EXPECT_CALL(Kite, _sendReq(_, kitepp::_methods::PUT, _, _, _))
+        .WillOnce(testing::Invoke([&jsonFWrap](rj::Document& data, const kitepp::_methods& mtd, const string& endpoint,
+                                      const std::vector<std::pair<string, string>>& bodyParams = {},
+                                      bool isJson = false) { data.ParseStream(jsonFWrap); }));
+
+    string orderID = Kite.modifyOrder("arg1", "arg2");
+
+    EXPECT_EQ(orderID, "151220000000000");
+}
+
+TEST(kiteTest, cancelOrderTest) {
+
+    std::ifstream jsonFile("../../tests/mock_responses/order_response.json");
+    ASSERT_TRUE(jsonFile);
+    rj::IStreamWrapper jsonFWrap(jsonFile);
+
+    mockKite Kite;
+
+    EXPECT_CALL(Kite, _sendReq(_, kitepp::_methods::DEL, _, _, _))
+        .WillOnce(testing::Invoke([&jsonFWrap](rj::Document& data, const kitepp::_methods& mtd, const string& endpoint,
+                                      const std::vector<std::pair<string, string>>& bodyParams = {},
+                                      bool isJson = false) { data.ParseStream(jsonFWrap); }));
+
+    string orderID = Kite.cancelOrder("arg1", "arg2");
+
+    EXPECT_EQ(orderID, "151220000000000");
+}
+
+TEST(kiteTest, exitOrderTest) {
+
+    std::ifstream jsonFile("../../tests/mock_responses/order_response.json");
+    ASSERT_TRUE(jsonFile);
+    rj::IStreamWrapper jsonFWrap(jsonFile);
+
+    mockKite Kite;
+
+    EXPECT_CALL(Kite, _sendReq(_, kitepp::_methods::DEL, _, _, _))
+        .WillOnce(testing::Invoke([&jsonFWrap](rj::Document& data, const kitepp::_methods& mtd, const string& endpoint,
+                                      const std::vector<std::pair<string, string>>& bodyParams = {},
+                                      bool isJson = false) { data.ParseStream(jsonFWrap); }));
+
+    string orderID = Kite.exitOrder("arg1", "arg2");
+
+    EXPECT_EQ(orderID, "151220000000000");
+}
