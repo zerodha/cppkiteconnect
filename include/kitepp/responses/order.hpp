1f97fcb0959ae1769cb67368fb72232944aec304
Bhumit Attarde <bhumit.attarde01@gmail.com>
2022-05-07 16:05:03 +0530

    refactor: mv order structs to a separate file

--- include/kitepp/responses/order.hpp
+++ include/kitepp/responses/order.hpp
@@ -1,17 +1,23 @@
+#pragma once
+
 #include <string>
 
 #include "../config.hpp"
+#include "../rjutils.hpp"
+#include "rapidjson/document.h"
+#include "rapidjson/rapidjson.h"
+
+namespace kiteconnect {
+
+using std::string;
+namespace rj = rapidjson;
+namespace kc = kiteconnect;
 
 #define GENERATE_FLUENT_METHOD(returnType, fieldType, fieldName, methodName)                                           \
     returnType& methodName(fieldType arg) {                                                                            \
         fieldName = arg;                                                                                               \
         return *this;                                                                                                  \
     };
-
-namespace kiteconnect {
-
-namespace kc = kiteconnect;
-using std::string;
 
 struct placeOrderParams {
     GENERATE_FLUENT_METHOD(placeOrderParams, int, quantity, Quantity);
@@ -45,6 +51,104 @@ struct placeOrderParams {
     string orderType;
     string validity;
     string tag;
+};
+
+/// order represents an individual order response
+struct order {
+    order() = default;
+    explicit order(const rj::Value::Object& val) { parse(val); };
+
+    void parse(const rj::Value::Object& val) {
+        kc::rjutils::_getIfExists(val, accountID, "account_id");
+        kc::rjutils::_getIfExists(val, placedBy, "placed_by");
+        kc::rjutils::_getIfExists(val, orderID, "order_id");
+        kc::rjutils::_getIfExists(val, exchangeOrderID, "exchange_order_id");
+        kc::rjutils::_getIfExists(val, parentOrderID, "parent_order_id");
+        kc::rjutils::_getIfExists(val, status, "status");
+        kc::rjutils::_getIfExists(val, statusMessage, "status_message");
+        kc::rjutils::_getIfExists(val, orderTimestamp, "order_timestamp");
+        kc::rjutils::_getIfExists(val, exchangeUpdateTimestamp, "exchange_update_timestamp");
+        kc::rjutils::_getIfExists(val, exchangeTimestamp, "exchange_timestamp");
+        kc::rjutils::_getIfExists(val, rejectedBy, "rejected_by");
+        kc::rjutils::_getIfExists(val, variety, "variety");
+        kc::rjutils::_getIfExists(val, exchange, "exchange");
+        kc::rjutils::_getIfExists(val, tradingSymbol, "tradingsymbol");
+        kc::rjutils::_getIfExists(val, instrumentToken, "instrument_token");
+        kc::rjutils::_getIfExists(val, orderType, "order_type");
+        kc::rjutils::_getIfExists(val, transactionType, "transaction_type");
+        kc::rjutils::_getIfExists(val, validity, "validity");
+        kc::rjutils::_getIfExists(val, product, "product");
+        kc::rjutils::_getIfExists(val, quantity, "quantity");
+        kc::rjutils::_getIfExists(val, disclosedQuantity, "disclosed_quantity");
+        kc::rjutils::_getIfExists(val, price, "price");
+        kc::rjutils::_getIfExists(val, triggerPrice, "trigger_price");
+        kc::rjutils::_getIfExists(val, averagePrice, "average_price");
+        kc::rjutils::_getIfExists(val, filledQuantity, "filled_quantity");
+        kc::rjutils::_getIfExists(val, pendingQuantity, "pending_quantity");
+        kc::rjutils::_getIfExists(val, cancelledQuantity, "cancelled_quantity");
+    };
+
+    int instrumentToken = kc::DEFAULTINT;
+    int quantity = kc::DEFAULTINT;
+    int disclosedQuantity = kc::DEFAULTINT;
+    int filledQuantity = kc::DEFAULTINT;
+    int pendingQuantity = kc::DEFAULTINT;
+    int cancelledQuantity = kc::DEFAULTINT;
+    double price = kc::DEFAULTDOUBLE;
+    double triggerPrice = kc::DEFAULTDOUBLE;
+    double averagePrice = kc::DEFAULTDOUBLE;
+    string accountID;
+    string placedBy;
+    string orderID;
+    string exchangeOrderID;
+    string parentOrderID;
+    string status;
+    string statusMessage;
+    string orderTimestamp;
+    string exchangeUpdateTimestamp;
+    string exchangeTimestamp;
+    string rejectedBy;
+    string variety;
+    string exchange;
+    string tradingSymbol;
+    string orderType;
+    string transactionType;
+    string validity;
+    string product;
+};
+
+/// trade represents an individual trade response
+struct trade {
+    trade() = default;
+    explicit trade(const rj::Value::Object& val) { parse(val); };
+
+    void parse(const rj::Value::Object& val) {
+        kc::rjutils::_getIfExists(val, averagePrice, "average_price");
+        kc::rjutils::_getIfExists(val, quantity, "quantity");
+        kc::rjutils::_getIfExists(val, tradeID, "trade_id");
+        kc::rjutils::_getIfExists(val, product, "product");
+        kc::rjutils::_getIfExists(val, fillTimestamp, "fill_timestamp");
+        kc::rjutils::_getIfExists(val, exchangeTimestamp, "exchange_timestamp");
+        kc::rjutils::_getIfExists(val, exchangeOrderID, "exchange_order_id");
+        kc::rjutils::_getIfExists(val, orderID, "order_id");
+        kc::rjutils::_getIfExists(val, transactionType, "transaction_type");
+        kc::rjutils::_getIfExists(val, tradingSymbol, "tradingsymbol");
+        kc::rjutils::_getIfExists(val, exchange, "exchange");
+        kc::rjutils::_getIfExists(val, InstrumentToken, "instrument_token");
+    };
+
+    int InstrumentToken = kc::DEFAULTINT;
+    double averagePrice = kc::DEFAULTDOUBLE;
+    double quantity = kc::DEFAULTDOUBLE;
+    string tradeID;
+    string product;
+    string fillTimestamp;
+    string exchangeTimestamp;
+    string exchangeOrderID;
+    string orderID;
+    string transactionType;
+    string tradingSymbol;
+    string exchange;
 };
 
 #undef GENERATE_FLUENT_METHOD
